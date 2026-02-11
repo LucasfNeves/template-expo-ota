6e247ed976e2f17a078b40b85ce5c69f21a57f02
Lucas Farias <lucasfarias.ln@gmail.com>
2026-02-11 20:08:00 -0300

    feat: adicionar cliente API com configuração de base URL

---
+++ src/shared/api/api-client.ts
@@ -0,0 +1,32 @@
+import axios, { AxiosInstance } from 'axios';
+import { Platform } from 'react-native';
+
+const getBaseUrl = () => {
+  return Platform.select({
+    ios: 'http://localhost:3000',
+    android: 'http://10.0.2.2:3001',
+    default: 'http://localhost:3001',
+  });
+};
+
+const baseURL = getBaseUrl();
+
+export class ApiClient {
+  private instance: AxiosInstance;
+  private isRefreshing = false;
+
+  constructor() {
+    this.instance = axios.create({
+      baseURL: baseURL,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+
+  getInstance() {
+    return this.instance;
+  }
+}
+
+export const apiClient = new ApiClient().getInstance();
