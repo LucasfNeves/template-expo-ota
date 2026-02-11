cc0c95a9107415c9ef8f8ae4026efdcac8ede6eb
Lucas Farias <lucasfarias.ln@gmail.com>
2026-02-11 20:06:03 -0300

    feat: simplificar lógica de redirecionamento com uso do componente Redirect

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,19 +1,11 @@
-import { router } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import { Text, TouchableOpacity, View } from 'react-native';
+import { Redirect } from 'expo-router';
+
+const token = null;
 
 export default function App() {
-  return (
-    <View className="flex-1 bg-white items-center justify-center p-8">
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+  if (!token) {
+    return <Redirect href="(public)/login" />;
+  }
 
-      <TouchableOpacity
-        className="bg-purple-base h-button w-full p-2 rounded-xl items-center justify-center"
-        onPress={() => router.push('/login')}
-      >
-        <Text className="text-white text-lg font-medium">Login</Text>
-      </TouchableOpacity>
-    </View>
-  );
+  return <Redirect href="/(private)/home" />;
 }
