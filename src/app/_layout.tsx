7e887bde9e1b4f498db2af2418b5be5d6164c3cd
Lucas Farias <lucasfarias.ln@gmail.com>
2026-02-11 20:04:00 -0300

    feat: reorganizar estrutura do layout e corrigir importação do queryClient

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,12 +1,16 @@
-import { Stack } from 'expo-router';
 import '@/styles/global.css';
+import { Stack } from 'expo-router';
 import { QueryClientProvider } from '@tanstack/react-query';
-import { queryClient } from '@/infrastructure/query/queryClient';
+import { queryClient } from '@/shared/queries/queryClient';
 
 export default function RootLayout() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Stack />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(public)/login" />
+        <Stack.Screen name="(public)/register" />
+        <Stack.Screen name="(private)" />
+      </Stack>
     </QueryClientProvider>
   );
 }
